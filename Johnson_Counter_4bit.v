83075c221427495431e7dc891f147b5585e573e0
T Tushar Shenoy <107348474+tusharshenoy@users.noreply.github.com>
2023-09-29 19:56:11 +0530

    Create Johnson_Counter_4bit.v

---
+++ Johnson_Counter_4bit.v
@@ -0,0 +1,16 @@
+module Johnson_Counter_4bit(clk, reset, Q);
+
+ input clk; 
+ input reset;
+ 
+
+ output [3:0] Q; 
+ 
+
+ wire Db3, Db2, Db1, Db0;
+
+ D_Flip_Flop DFF_1(.D(Db0), .reset(reset), .clk(clk), .Q(Q[3]), .Qb(Db3)); 
+ D_Flip_Flop DFF_2(.D(Q[3]), .reset(reset), .clk(clk), .Q(Q[2]), .Qb(Db2)); 
+ D_Flip_Flop DFF_3(.D(Q[2]), .reset(reset), .clk(clk), .Q(Q[1]), .Qb(Db1));
+ D_Flip_Flop DFF_4(.D(Q[1]), .reset(reset), .clk(clk), .Q(Q[0]), .Qb(Db0)); 
+endmodule
